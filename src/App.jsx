5b770b3071f050abf5493d4a6e085c3639b72c29
Priti Chauhan <114865373+Preeti980@users.noreply.github.com>
2024-04-07 18:44:15 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
-import Profile from './pages/Profile';
 export default function App(){
   return (
     <BrowserRouter>
